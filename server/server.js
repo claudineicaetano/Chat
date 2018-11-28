e6c49a069df3f207d7b04fe83f8af7c07ef18f5b
Claudinei Caetano de Souza <claudinei@angularlabs.com.br>
2018-11-28 18:53:11 -0200

    Create generateMessage ultility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,29 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-      from: 'Mananger',
-      text: 'Welcome to the ower chat!'
-  });
+  socket.emit('newMessage', generateMessage('Administrator', 'Welcome to the ower chat!'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Administrator',
-    text: 'New user Joined us',
-    createAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Adm', 'New user Joined us'));
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-        from: message.from,
-        text: message.text,
-        createAt: new Date().getTime()
-    });
-    // socket.broadcast.emit({
-    //   from: message.from,
-    //   text: message.text,
-    //   createAt: new Date().getTime()
-    // })
+     io.emit('newMessage', generateMessage(message.from, message.text))
   });
 
   socket.on('disconnect', () => {
