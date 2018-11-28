81a1da2a570ce287d4a04bfc78670b6160617353
Claudinei Caetano de Souza <claudinei@angularlabs.com.br>
2018-11-28 17:46:34 -0200

    Adjust on Emit newMessage on crateMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connect to server');
-  
-  socket.emit('createMessage', {
-    from: 'Claudinei',
-    text: 'Hi, hru?'
-  });
 });
 
 socket.on( 'disconnect', function () {
