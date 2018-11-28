1afcf42c76a8685a03f0aeff58747f2fa9e4fd30
Claudinei Caetano de Souza <claudinei@angularlabs.com.br>
2018-11-28 18:04:21 -0200

    Alerts and greets new users

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+      from: 'Mananger',
+      text: 'Welcome to the ower chat!'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Administrator',
+    text: 'New user Joined us',
+    createAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -21,6 +32,11 @@ io.on('connection', (socket) => {
         text: message.text,
         createAt: new Date().getTime()
     });
+    // socket.broadcast.emit({
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // })
   });
 
   socket.on('disconnect', () => {
