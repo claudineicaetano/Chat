d89f13ac2d8fbab118b7a201cebcdd303c333323
Claudinei Caetano de Souza <claudinei@angularlabs.com.br>
2018-11-30 12:50:29 -0200

    Add geolocation support via google maps api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -12,4 +12,17 @@ describe('generateMessage', () => {
     expect(message).toMatchObject({from, text});
 
   });
+}); 
+
+describe('generateLocationMessage', () => {
+  it('hould generate correct location Object', () => {
+    var from = 'Alice';
+    var latitude = 100;
+    var longitude = 100;
+    var url = 'https://www.google.com/maps?q=100,100';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(typeof message.createAt).toBe('number');
+    expect(message).toMatchObject({from, url});
+  }); 
 });
